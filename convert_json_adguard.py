d5ca2d5d177d7d7e0c0458d2b904cd0de0804ab4
022by <46434255+pplulee@users.noreply.github.com>
2023-02-15 11:21:51 +0000

    更新处理方法，重复出现的域名设置成通配符

--- convert_json_adguard.py
+++ convert_json_adguard.py
@@ -1,11 +1,43 @@
 import json
+import time
 
 file = open('input.json', 'r', encoding='utf-8')
 json_data = json.load(file)
 file.close()
 output = open('tmp-mail.txt', 'w', encoding='utf-8')
 print(f"共有{len(json_data)}条数据")
+
+domain_cache = []
+domain_list = []
+special_domain = []
+
+
+class Domain:
+    def __init__(self, name, suffix):
+        self.name = name
+        self.suffix = suffix
+
+    def set_wildcard_suffix(self):
+        self.suffix = "*"
+
+
+start_time = time.time()
 for item in json_data:
+    if item.count('.') == 1:
+        domain, suffix = item.split('.')
+        if domain not in domain_cache:
+            domain_cache.append(domain)
+            domain_list.append(Domain(domain, suffix))
+        else:
+            domain_list[domain_cache.index(domain)].set_wildcard_suffix()
+    else:
+        # 特殊域名，跳过
+        special_domain.append(item)
+end_time = time.time()
+print(f"处理完成，耗时{end_time - start_time}秒")
+for item in domain_list:
+    output.write(f"||{item.name}.{item.suffix}^\n")
+for item in special_domain:
     output.write(f"||{item}^\n")
 output.close()
 print("转换完成")
