ae9fb1baa77c834a2cfcf5763c9fa9a3accfce58
022by <46434255+pplulee@users.noreply.github.com>
2023-02-16 10:49:37 +0000

    Update source file
    Add allow list

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-屏蔽临时邮箱域名，规则适用于Adguard Home
-输入数据来自项目 ivolo/disposable-email-domains
+屏蔽临时邮箱域名，规则适用于Adguard Home \
+
+输入数据来源： https://github.com/amieiro/disposable-email-domains
+
+
--- convert_json_adguard.py
+++ convert_json_adguard.py
@@ -1,11 +1,20 @@
 import json
 
-file = open('input.json', 'r', encoding='utf-8')
-json_data = json.load(file)
-file.close()
-output = open('tmp-mail.txt', 'w', encoding='utf-8')
-print(f"共有{len(json_data)}条数据")
-for item in json_data:
-    output.write(f"||{item}^\n")
+block_file = open('block.json', 'r', encoding='utf-8')
+block_json_data = json.load(block_file)
+block_file.close()
+output = open('rule.txt', 'w', encoding='utf-8')
+print(f"共有{len(block_json_data)}条封禁数据")
+for item in block_json_data:
+    if item!="" and "#" not in item:
+        output.write(f"||{item}^\n")
+
+allow_file = open('allow.json', 'r', encoding='utf-8')
+allow_json_data = json.load(allow_file)
+allow_file.close()
+print(f"共有{len(allow_json_data)}条允许数据")
+for item in allow_json_data:
+    if item!="" and "#" not in item:
+        output.write(f"@@||{item}^\n")
 output.close()
 print("转换完成")
